cd44e0f2dd85823840990bc690dbf5800280fafc
Leon Ramiro <racramiro@gmail.com>
2021-01-28 20:45:40 -0500

    [update] responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { Row, Col } from "antd";
 import { NavBar } from "../components/NavBar";
-import Voting from "../components/Voting";
 import { SectionMid } from "../components/SectionMid";
 import { ContentCards } from "../components/ContentCards";
 
@@ -9,13 +7,6 @@ export const Home = () => {
   return (
     <>
       <NavBar />
-      {/* <Row>
-        <Col lg={2} xs={0}></Col>
-        <Col xl={10} lg={10} xs={24}>
-          <Voting />
-        </Col>
-      </Row> */}
-
       <SectionMid />
       <ContentCards />
     </>
